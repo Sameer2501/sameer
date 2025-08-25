5545dfe0d93ed5ae55f8ba8b702f5f6e603a4a09
Sameer2501 <sameeransari87288@gmail.com>
2025-08-25 14:52:59 +0530

    nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,1 +1,9 @@
-import nodemailer from 'nodmailer'
+import nodemailer from 'nodemailer'
+ const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+export default transporter;
